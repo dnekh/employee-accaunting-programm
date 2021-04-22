88d9644758d5a4c9d2bda51c54edeaec86318099
d_nekh <dnekh2000@gmail.com>
2021-04-23 04:03:10 +0900

    latest version that will be sent for exam

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         Employee employee = new Employee();
+        Programmer programmer = new Programmer();
+        Designer designer = new Designer();
         Employee empProg = new Programmer("Meow Moore", "Программист", 30, 5,
                 100_000, "Java");
         Employee empDes = new Designer("Super Mors", "Дизайнер", 25, 3,
                 70_000, "Adobe Illustrator");
-        //employee.getFullTextInfo();
         empProg.getFullTextInfo();
         System.out.println();
         empDes.getFullTextInfo();
